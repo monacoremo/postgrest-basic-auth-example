b500a6eff9a417de27c282bf8830a32b9413b74f
Remo <remo>
2020-05-28 15:05:43 +0200

    Nix basics

--- default.nix
+++ default.nix
@@ -1,1 +1,28 @@
-{}
+let
+  nixpkgsVersion =
+    {
+      date = "2020-05-21";
+      rev = "6405edf2dca7f6faaa29266136dfa7f8f969b511";
+      tarballHash = "069skn0ayxmhdlw1xcj92cij7wydkk2bndkcyk4vvhms16p9wj46";
+    };
+
+  nixpkgs =
+    builtins.fetchTarball {
+      url = "https://github.com/nixos/nixpkgs/archive/${nixpkgsVersion.rev}.tar.gz";
+      sha256 = nixpkgsVersion.tarballHash;
+    };
+
+  pkgs =
+    import nixpkgs { };
+in
+{
+  run =
+    pkgs.writeShellScriptBin "run"
+      ''
+      '';
+
+  test =
+    pkgs.writeShellScriptBin "test"
+      ''
+      '';
+}
